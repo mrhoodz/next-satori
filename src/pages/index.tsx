6e83e4f3ddd6ca2b008e8b6d724a1e260c5f7710
mrhoodz <hoodzt7@gmail.com>
2023-06-08 00:16:01 +0200

    push it

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,8 +14,9 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div>hello motto</div>
+      <h1>hello motto</h1>
       <img src="/api/og" alt="dynamic image goes here" />
+      <p>asdasd</p>
     </>
   );
 }
